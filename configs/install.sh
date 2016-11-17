a163416851cfa9ccbeee7dab7f910fdd861b6365
Jeff <Jffrypwll@googlemail.com>
2016-11-17 23:45:46 +0000

    Update install.sh

--- configs/install.sh
+++ configs/install.sh
@@ -3,7 +3,7 @@
 # configure apache
 cp /home/pi/pi-heating-remote/configs/homeheating-site.conf /etc/apache2/sites-available/homeheating-site.conf
 chown root:root /etc/apache2/sites-available/homeheating-site.conf
-chmod 744 /etc/apache2/sites-available/homeheating-site.conf
+chmod 644 /etc/apache2/sites-available/homeheating-site.conf
 a2ensite homeheating-site
 service apache2 reload
 
