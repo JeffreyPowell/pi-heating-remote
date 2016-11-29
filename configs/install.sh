50f43569e16367a11641809e8adbc4f8a39d4b82
Jeff <Jffrypwll@googlemail.com>
2016-11-29 23:28:48 +0000

    Delete install.sh

--- configs/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# configure apache
-cp /home/pi/pi-heating-remote/configs/homeheating-site.conf /etc/apache2/sites-available/homeheating-site.conf
-chown root:root /etc/apache2/sites-available/homeheating-site.conf
-chmod 644 /etc/apache2/sites-available/homeheating-site.conf
-a2ensite homeheating-site
-service apache2 reload
-
