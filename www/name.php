ac6c44b8b1a51b99fc2aba69f626440229d0e474
Jeff <Jffrypwll@googlemail.com>
2016-12-13 21:55:37 +0000

    Create name.php

---
+++ www/name.php
@@ -0,0 +1,25 @@
+<?php
+
+$SENSOR_ID = isset($_GET['id']) ? $_GET['id'] : '0';
+
+if ( $SENSOR_ID = 0 ) {
+
+    print_r ( '' );
+
+  }
+    else
+  {
+  
+    $sensorfile = '/home/pi/pi-heating-remote/configs/sensors';
+
+    $sensors = file( $sensorfile );
+
+    $sensor = explode( " = ", $sensors[$SENSOR_ID-1] );
+
+    $name=$sensor[1];
+
+    print_r ( $name);
+    
+  }
+  
+?>
