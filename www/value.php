7d28a3c596f6bc70ee361bd8e933dcc76f306549
Jeff <Jffrypwll@googlemail.com>
2016-12-13 22:04:15 +0000

    Update value.php

--- www/value.php
+++ www/value.php
@@ -1,23 +1,37 @@
 <?php
+
 $SENSOR_ID = isset($_GET['id']) ? $_GET['id'] : '0';
 
-$w1dir = "/sys/bus/w1/devices/";
-$sensorfile = '/home/pi/pi-heating-remote/configs/sensors';
-//echo "<html><body><pre>";
-$sensors = file($sensorfile, FILE_SKIP_EMPTY_LINES);
+if ( $SENSOR_ID = 0 ) {
 
-$sensor = explode( " = ", $sensors[$SENSOR_ID] );
+    print_r ( '' );
 
-$ref=$sensor[0];
-$name=$sensor[1];
+  }
+    else
+  {
+      
+    $w1dir = "/sys/bus/w1/devices/";
+    
+    $sensorfile = '/home/pi/pi-heating-remote/configs/sensors';
+
+    $sensors = file($sensorfile, FILE_SKIP_EMPTY_LINES);
+
+    $sensor = explode( " = ", $sensors[$SENSOR_ID-1] );
+
+    $ref=$sensor[0];
+    //$name=$sensor[1];
   
-  do {
+    do {
       $raw_data = file_get_contents($w1dir.$ref.'/w1_slave');
       $success = substr( explode( "\n", $raw_data )[0], -3, 3 );
     } while ( $success !="YES" ); 
     
-  $value = (float)substr( explode( "\n", $raw_data )[1], -5, 5 ) / 1000.0;
-  echo "".$ref."=".$name."=".$value.",";
+    $value = (float)substr( explode( "\n", $raw_data )[1], -5, 5 ) / 1000.0;
 
-//echo "</pre></body></html>";
+    print_r( $value );
+    
+    }
+
 ?>
+
+<?php
